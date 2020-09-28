0e7ff3477ad4b1c08e4779a1d88cda48c42757f0
Benjamin Klebel <bklebel@users.noreply.github.com>
2020-09-28 18:45:40 +0200

    docs update

--- measureSequences/runSequences.py
+++ measureSequences/runSequences.py
@@ -62,7 +62,7 @@ def mapping_tofunc(func, start: float, end: float, Nsteps: int) -> "type(np.arra
 
 
 class Sequence_runner:
-    """docstring for Sequence_Thread"""
+    """docstring for Sequence_runner"""
 
     def __init__(
         self,
